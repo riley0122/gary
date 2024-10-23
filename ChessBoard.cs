5eb32eb253e288e498624d544121b25a62e3b9a9
Riley <77288737+riley0122@users.noreply.github.com>
2024-10-23 18:39:49 +0200

    implement fen loading

--- ChessBoard.cs
+++ ChessBoard.cs
@@ -40,5 +40,51 @@ namespace Gary
             }
             return boardString;
         }
+
+        public void LoadFromFEN(string fen) {
+            string[] parts = fen.Split("  ");
+            string pieces = parts[0];
+            string[] ranks = pieces.Split("/");
+
+            for (int rank = 8; rank >= 1; rank--) {
+                int fileIndex = 0;
+                for (int i = 0; i < ranks[8 - rank].Length; i++) {
+                    char symbol = ranks[8 - rank][i];
+
+                    if (char.IsDigit(symbol)) {
+                        // Empty squares
+                        fileIndex += symbol - '0';
+                    } else {
+                        char file = (char)('a' + fileIndex);
+                        Square square = new Square(file, rank);
+
+                        IPiece piece = CreatePieceFromSymbol(symbol, square);
+                        if (piece != null) {
+                            PlacePiece(piece, square);
+                        }
+                        fileIndex++;
+                    }
+                }
+            }
+        }
+
+        private IPiece CreatePieceFromSymbol(char symbol, Square square) {
+            switch (symbol)
+            {
+                case 'P': return new Pawn(square, true); // White pawn
+                case 'p': return new Pawn(square, false); // Black pawn
+                case 'R': return new Rook(square, true); // White rook
+                case 'r': return new Rook(square, false); // Black rook
+                case 'N': return new Knight(square, true); // White knight
+                case 'n': return new Knight(square, false); // Black knight
+                case 'B': return new Bishop(square, true); // White bishop
+                case 'b': return new Bishop(square, false); // Black bishop
+                case 'Q': return new Queen(square, true); // White queen
+                case 'q': return new Queen(square, true); // Black queen
+                case 'K': return new King(square, true); // White king
+                case 'k': return new King(square, true); // Black king
+                default: return null;
+            }
+        }
     }
 }
