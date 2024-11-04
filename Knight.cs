2c4c2cf1730ce22f391ba25e7d787721d883d1af
Riley <77288737+riley0122@users.noreply.github.com>
2024-11-04 17:00:03 +0100

    Implement knight movement

--- Knight.cs
+++ Knight.cs
@@ -13,13 +13,40 @@ namespace Gary
         }
 
         public bool IsMoveValid(Square targetSquare) {
-            // TODO: implement correct movement validation
-            return true;
+            int rankDelta = Math.Abs(targetSquare.rank - this.CurrentPosition.rank);
+            int fileDelta = Math.Abs(targetSquare.file - this.CurrentPosition.file);
+
+            if (this.CurrentPosition.board.GetPieceAt(targetSquare) is not null && this.CurrentPosition.board.GetPieceAt(targetSquare).isWhite == this.isWhite) {
+                return false;
+            }
+
+            return (rankDelta == 2 && fileDelta == 1) || (rankDelta == 1 && fileDelta == 2);
         }
 
         public Square[] GetValidMoves() {
-            // TODO: imlpement getting moves
-            return [];
+            List<Square> legalMoves = new List<Square>();
+            ChessBoard board = this.CurrentPosition.board;
+
+            (int relativeRank, int relativeFile)[] relativePositions = new (int, int)[] {
+                (2, 1), (2, -1),
+                (-2, 1), (-2, -1),
+                (1, 2), (-1, 2),
+                (1, -2), (-1, -2)
+            };
+
+            foreach ((int relativeRank, int relativeFile) in relativePositions)
+            {
+                int targetRank = CurrentPosition.rank + relativeRank;
+                char targetFile = (char)(CurrentPosition.file + relativeFile);
+                if (targetFile < 'a' || targetFile > 'h' || targetRank < 1 || targetRank > 8) continue;
+
+                Square target = new Square(targetFile, targetRank, board);
+                if (IsMoveValid(target)) {
+                    legalMoves.Add(target);
+                }
+            }
+
+            return legalMoves.ToArray();
         }
 
         public string GetPieceSymbol() {
