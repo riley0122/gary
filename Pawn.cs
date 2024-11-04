774e06c56e8c493c348593f7df8e94b07817889f
Riley <77288737+riley0122@users.noreply.github.com>
2024-11-04 13:50:02 +0100

    Reworked IsMoveValid

--- Pawn.cs
+++ Pawn.cs
@@ -14,60 +14,29 @@ namespace Gary
 
         public bool IsMoveValid(Square targetSquare) {
             ChessBoard board = targetSquare.board;
-
             IPiece? targetPiece = board.GetPieceAt(targetSquare);
 
-            // If there is a piece of the same colour on the target square
-            if (targetPiece is not null && targetPiece.isWhite == this.isWhite) {
-                return false;
-            }
+            int rankDelta = targetSquare.rank - this.CurrentPosition.rank;
+            int fileDelta = targetSquare.file - this.CurrentPosition.file;
 
-            // If there is a piece already on the target piece and the piece is within the same file (vertical)
-            if (targetPiece is not null && targetPiece.CurrentPosition.file == this.CurrentPosition.file) {
-                return false;
-            }
-
-            // If the target square is not in a rank (horizontal) next to the current one
-            if (Math.Max(targetSquare.rank, this.CurrentPosition.rank) - Math.Min(targetSquare.rank, (int)this.CurrentPosition.rank) != 1) {
-                return false;
-            }
-
-            // If the file is not the same and there is not a piece that is the same colour
-            if (targetSquare.file != this.CurrentPosition.file && targetPiece is not null && targetPiece.isWhite == this.isWhite) {
-                return false;
-            }
-
-            // If the target piece exists and it's file is the same or its rank is not one higher or lower
-            if (targetPiece is not null && (targetSquare.file == this.CurrentPosition.file || Math.Max(targetSquare.rank, this.CurrentPosition.rank) - Math.Min(targetSquare.rank, this.CurrentPosition.rank) != 1)) {
-                return false;
-            }
-
-            // rank diffrences
-            if (this.isWhite) {
-                // Is starting position
-                if (this.CurrentPosition.rank == 2) {
-                    if (targetSquare.rank - this.CurrentPosition.rank != 1 && targetSquare.rank - this.CurrentPosition.rank != 2) {
-                        return false;
-                    }
-                } else {
-                    if (targetSquare.rank - this.CurrentPosition.rank != 1) {
-                        return false;
-                    }
+            if (fileDelta == 0) {
+                if (this.isWhite && rankDelta == 1 || !this.isWhite && rankDelta == -1) {
+                    return targetPiece is null;
                 }
-            } else {
-                // Is starting position
-                if (this.CurrentPosition.rank == 7) {
-                    if (targetSquare.rank - this.CurrentPosition.rank != 1 && targetSquare.rank - this.CurrentPosition.rank != 2) {
-                        return false;
-                    }
-                } else {
-                    if (targetSquare.rank - this.CurrentPosition.rank != 1) {
-                        return false;
-                    }
+
+                if (this.isWhite && rankDelta == 2 && this.CurrentPosition.rank == 2 || !this.isWhite && rankDelta == -2 && this.CurrentPosition.rank == 7) {
+                    Square inbetween = new Square(this.CurrentPosition.file, this.CurrentPosition.rank + rankDelta / 2, board);
+                    return targetPiece is null && board.GetPieceAt(inbetween) is null;
                 }
+
+                return false;
             }
 
-            return true;
+            if (Math.Abs(fileDelta) == 1 && (this.isWhite && rankDelta == 1 || !this.isWhite && rankDelta == -1)) {
+                return targetPiece is not null && targetPiece.isWhite != this.isWhite;
+            }
+
+            return false;
         }
 
         public Square[] GetValidMoves() {
